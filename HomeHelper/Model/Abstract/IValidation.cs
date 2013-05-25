d31c6eb42711e85277daf14872deb79468ed19a5
Revan <Revan@.(none)>
2013-05-25 17:27:37 +0300

    Adaugat un nou namespace abstract in model, respectiv introdus validarea.

---
+++ HomeHelper/Model/Abstract/IValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeHelper.Model.Abstract
+{
+    public interface IValidation
+    {
+        void DoValidation();
+        Dictionary<string, string> Errors { get; }
+    }
+}
