9d369a0a4e22e0e90be660ad2604211691060492
Revan <Revan@.(none)>
2013-10-07 21:58:51 +0300

    Resource consumeru din wp

---
+++ HomeHelperPhone/Utils/PhoneResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomeHelper.Model.Abstract;
+using HomeHelperPhone.Resources;
+
+namespace HomeHelperPhone.Utils
+{
+    public class PhoneResources:IResources 
+    {
+
+        public string GetString(string resource)
+        {
+            return AppResources.ResourceManager.GetString(resource);
+        }
+    }
+}
