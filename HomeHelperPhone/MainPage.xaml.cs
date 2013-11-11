e739fae3d4cc2c6540318205cbc37a8051882547
Revan <Revan@.(none)>
2013-11-11 23:40:04 +0200

    Adaugat un pivot view pentru consum

--- HomeHelperPhone/MainPage.xaml.cs
+++ HomeHelperPhone/MainPage.xaml.cs
@@ -58,13 +58,22 @@ namespace HomeHelperPhone
         }
 
 
+
+        private void EditItemClick(object sender, RoutedEventArgs e)
+        {
+            var item = lstUtilitati.SelectedItem as Utilitati;
+            if (item == null) return;
+            if (e == null) return;
+            NavigationService.Navigate(new Uri(string.Format("/Views/EditViewUtilitate.xaml?Id={0}",item.IdUtilitati), UriKind.Relative));
+        }
+
         private void LstUtilitati_OnTap(object sender, GestureEventArgs e)
         {
             var item = (e.OriginalSource as FrameworkElement).DataContext as Utilitati;
             if (item == null) return;
             if (e == null) return;
-            NavigationService.Navigate(new Uri(string.Format("/Views/EditViewUtilitate.xaml?Id={0}",item.IdUtilitati), UriKind.Relative));
-            
+            NavigationService.Navigate(new Uri(string.Format("/Views/ViewConsumuri.xaml?Id={0}", item.IdUtilitati), UriKind.Relative));
+
         }
     }
 }
---
+++ HomeHelperPhone/Views/ViewConsumuri.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using HomeHelper.Model;
+using HomeHelper.Repository.Abstract;
+using HomeHelper.Utils;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace HomeHelperPhone.Views
+{
+    public partial class ViewConsumuri : PhoneApplicationPage
+    {
+        private Utilitati _utilitati;
+        private ObservableCollection<ConsumUtilitate> _consumUtilitates;
+        private readonly IRepository<Utilitati> _repository = FactoryRepository.GetInstanceRepositoryUtilitati();
+        private readonly IRepository<ConsumUtilitate> _repositoryConsum = FactoryRepository.GetInstanceRepositoryConsum(); 
+
+        public ViewConsumuri()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var param = string.Empty;
+            if (NavigationContext.QueryString.TryGetValue("Id", out param))
+            {
+                _utilitati = _repository.GetAll().FirstOrDefault(a => a.IdUtilitati == int.Parse(param));
+               
+            }
+            else
+            {
+                _utilitati = new Utilitati();
+            }
+            DataContext = _utilitati;
+
+        }
+
+    }
+}
