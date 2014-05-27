c8e43d91f96d9a05fadd81148b3e3729dac0ac5c
Revan <Revan@.(none)>
2014-05-27 21:45:03 +0300

    Facut metodele cleanup la fotografiile nefolosite din application storage.

--- HomeHelperPhone/Utils/IoUtils.cs
+++ HomeHelperPhone/Utils/IoUtils.cs
@@ -5,6 +5,7 @@ using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeHelper.Repository.Concret;
 using Microsoft.Phone.Tasks;
 using Windows.Storage;
 
@@ -46,12 +47,13 @@ namespace HomeHelperPhone.Utils
             if (!imgToBeDelete.Any()) return;
             using (var isolatedStorage=IsolatedStorageFile.GetUserStoreForApplication())
             {
-                var files = isolatedStorage.GetFileNames().Select(a => new
+                var files = isolatedStorage.GetFileNames(@"Photos\*").Select(a => new
                                                                            {
                                                                                Folder = Path.GetDirectoryName(a),
                                                                                FileName = Path.GetFileName(a),
-                                                                               FullPath = a
-                                                                           }).Where(a=>a.Folder.ToLower().Equals("photos")).ToList();
+                                                                               FullPath = Path.Combine("Photos",a),
+                                                                               Extension=Path.GetExtension(a)
+                                                                           }).Where(a=>!a.Extension.ToLower().Contains("sql")).ToList();
                 foreach (var file in files)
                 {
                     if ((imgToBeDelete.Any(a => a.ToLower().Equals(file.FileName.ToLower()))
@@ -66,11 +68,20 @@ namespace HomeHelperPhone.Utils
             }
         }
 
-        public static IEnumerable<string> GetFiles()
+        public static void CleanupUnusedImages()
+        {
+            var repo = new ConsumUtilitateRepository();
+            var listUsed = repo.GetAll().Select(a => Path.GetFileName(a.ImagePath)).Where(a=>!string.IsNullOrEmpty(a)).ToList();
+            var files = GetFilesForPhotos().Where(a => !string.IsNullOrEmpty(a) && !Path.GetExtension(a).ToLower().Contains("sql")).ToList();
+            var delete = files.Where(a=>!listUsed.Contains(a)).ToList();
+            DeleteImages(delete);
+        }
+
+        public static IEnumerable<string> GetFilesForPhotos()
         {
             using (var isolatedStorage=IsolatedStorageFile.GetUserStoreForApplication())
             {
-                return isolatedStorage.GetFileNames();
+                return isolatedStorage.GetFileNames(@"Photos\*");
             }
         }
 
